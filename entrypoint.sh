774ec7c81656cb195d16af9d4251feb0aa987031
Nikhil Chavan <email@nikhilchavan.com>
2019-09-12 13:35:18 +0530

    Revert deleted commands for deploying the plugin

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,3 +33,85 @@ if [[ -z "$VERSION" ]]; then
 fi
 echo "ℹ︎ VERSION is $VERSION"
 
+if [[ -z "$ASSETS_DIR" ]]; then
+	ASSETS_DIR=".wordpress-org"
+fi
+echo "ℹ︎ ASSETS_DIR is $ASSETS_DIR"
+
+SVN_URL="http://plugins.svn.wordpress.org/${SLUG}/"
+SVN_DIR="/github/svn-${SLUG}"
+
+# Checkout just trunk and assets for efficiency
+# Tagging will be handled on the SVN level
+echo "➤ Checking out .org repository..."
+svn checkout --depth immediates "$SVN_URL" "$SVN_DIR"
+cd "$SVN_DIR"
+svn update --set-depth infinity assets
+svn update --set-depth infinity trunk
+
+echo "➤ Copying files..."
+if [[ -e "$GITHUB_WORKSPACE/.distignore" ]]; then
+	echo "ℹ︎ Using .distignore"
+	# Copy from current branch to /trunk, excluding dotorg assets
+	# The --delete flag will delete anything in destination that no longer exists in source
+	rsync -rc --exclude-from="$GITHUB_WORKSPACE/.distignore" "$GITHUB_WORKSPACE/" trunk/ --delete
+else
+	echo "ℹ︎ Using .gitattributes"
+
+	cd "$GITHUB_WORKSPACE"
+
+	# "Export" a cleaned copy to a temp directory
+	TMP_DIR="/github/archivetmp"
+	mkdir "$TMP_DIR"
+
+	git config --global user.email "10upbot+github@10up.com"
+	git config --global user.name "10upbot on GitHub"
+
+	# If there's no .gitattributes file, write a default one into place
+	if [[ ! -e "$GITHUB_WORKSPACE/.gitattributes" ]]; then
+		cat > "$GITHUB_WORKSPACE/.gitattributes" <<-EOL
+		/$ASSETS_DIR export-ignore
+		/.gitattributes export-ignore
+		/.gitignore export-ignore
+		/.github export-ignore
+		EOL
+
+		# Ensure we are in the $GITHUB_WORKSPACE directory, just in case
+		# The .gitattributes file has to be committed to be used
+		# Just don't push it to the origin repo :)
+		git add .gitattributes && git commit -m "Add .gitattributes file"
+	fi
+
+	# This will exclude everything in the .gitattributes file with the export-ignore flag
+	git archive HEAD | tar x --directory="$TMP_DIR"
+
+	cd "$SVN_DIR"
+
+	# Copy from clean copy to /trunk, excluding dotorg assets
+	# The --delete flag will delete anything in destination that no longer exists in source
+	rsync -rc "$TMP_DIR/" trunk/ --delete
+fi
+
+# Copy dotorg assets to /assets
+rsync -rc "$GITHUB_WORKSPACE/$ASSETS_DIR/" assets/ --delete
+
+# Add everything and commit to SVN
+# The force flag ensures we recurse into subdirectories even if they are already added
+# Suppress stdout in favor of svn status later for readability
+echo "➤ Preparing files..."
+svn add . --force > /dev/null
+
+# SVN delete all deleted files
+# Also suppress stdout here
+svn status | grep '^\!' | sed 's/! *//' | xargs -I% svn rm % > /dev/null
+
+# Copy tag locally to make this a single commit
+echo "➤ Copying tag..."
+svn cp "trunk" "tags/$VERSION"
+
+svn status
+
+echo "➤ Committing files..."
+svn commit -m "Update to version $VERSION from GitHub" --no-auth-cache --non-interactive  --username "$SVN_USERNAME" --password "$SVN_PASSWORD"
+
+echo "✓ Plugin deployed!"
